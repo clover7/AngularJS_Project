2610faca80ace6b30ea5101314ee2c2df084f33e
clover7 <heaun37@gmail.com>
2016-06-01 10:31:46 +0900

    9. todo http example : edit delete api and add update api

--- week_2/controller/todoController.js
+++ week_2/controller/todoController.js
@@ -2,35 +2,50 @@
 //      alert("todoController"); 
  
        $scope.deleteTodo = function(todo){
-          alert(todo._id.$oid); 
-           var id = todo._id.$oid;
+          //alert(todo._id.$oid);
            var promise = $http({
-              url: "https://api.mlab.com/api/1/databases/angular/collections/todos/:id",
+              url: "https://api.mlab.com/api/1/databases/angular/collections/todos/"+todo._id.$oid,
               method: "DELETE",
               params: {
                   "apiKey": "0m3TAU3qpXHvc9LRN5JgYSCuZbvKS5N6"
               } 
           });
- 
-           
+
           promise.error(function (data, status, header, config) {
-              alert("error : " + status);
+              alert("error : " + status + " /" + data);
                $scope.loader = false;
           });
           promise.success(function (data, status, header, config) {
 //              alert(data);
             $scope.todos = data;
             $scope.loader = true;
+            getTodos();
               
-          }); 
-           
-      } 
-       
+          });
+      }
       //ui상에서 호출할 필요 없으므로.
+
+      $scope.updateTodo = function(todo){
+          var promise = $http({
+                  url: "https://api.mlab.com/api/1/databases/angular/collections/todos/"+todo._id.$oid,
+              method: "PUT",
+              params:{
+                  "apiKey": "0m3TAU3qpXHvc9LRN5JgYSCuZbvKS5N6"
+              },
+              data:todo
+          });
+
+          promise.error(function(data, status, header, config){
+              alert("error :" + status + " / "+ data);
+          });
+          promise.success(function(data, status, header, config){
+              getTodos();
+          });
+
+      }
       function getTodos() {
           $scope.todos = [];
           $scope.loader = false;
-
           var promise = $http({
               url: "https://api.mlab.com/api/1/databases/angular/collections/todos",
               method: "GET",
@@ -40,18 +55,15 @@
           });
 
           promise.error(function (data, status, header, config) {
-              alert("error : " + status);
+              alert("error : " + status + " /" + data);
                $scope.loader = false;
           });
           promise.success(function (data, status, header, config) {
-//              alert(data);
+              $scope.loader = true;
               $scope.todos = data;
-            $scope.loader = true;
-              
           }); 
       }
+
       getTodos();
-      
-      
      
 }]); 
--- week_2/controller/writeController.js
+++ week_2/controller/writeController.js
@@ -1,25 +1,32 @@
 app.controller("writeController", ["$scope", "$http", "$location",  function ($scope, $http, $location) {
      // alert("writeController");  
       $scope.writeTodo = function(title) { 
-          alert(title);
-          var todo = {"title":title, "end":false}; 
-          var promise = $http({
-              url: "https://api.mlab.com/api/1/databases/angular/collections/todos",
-              method: "POST",
-              params: {
-                  "apiKey": "0m3TAU3qpXHvc9LRN5JgYSCuZbvKS5N6"
-              },
-              data : todo //서버로 넘길 데이터
-          });
+          //alert(title);
+          if(title==undefined)
+             alert("please input today's todo List.");
+          else{
+              //alert(title);
+              var todo = {"title":title, "end":false};
 
-          promise.error(function (data, status, header, config) {
-              alert("error : " + status);
-               $scope.loader = false;
-          });
-          promise.success(function (data, status, header, config) {
-//              alert(data);  
-              $location.path("/todo");
-          }); 
+              var promise = $http({
+                  url: "https://api.mlab.com/api/1/databases/angular/collections/todos",
+                  method: "POST",
+                  params: {
+                      "apiKey": "0m3TAU3qpXHvc9LRN5JgYSCuZbvKS5N6"
+                  },
+                  data : todo //서버로 넘길 데이터
+              });
+
+              promise.error(function (data, status, header, config) {
+                  alert("error : " + status);
+                  $scope.loader = false;
+              });
+              promise.success(function (data, status, header, config) {
+//              alert(data);
+                  $location.path("/todo");
+              });
+          }
+          $scope.title = "";
       }
-       $scope.title = ""; 
+
 }]);
